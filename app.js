f9a11de00acbc98a00dd4c911d1a369c0b61ee08
Mandeep-Parmar <mandeeppar07@gmail.com>
2026-02-07 15:35:43 +0530

    Initial setup: MongoDB connected, Listing schema created, initial app running

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Listing = require("./models/listing.js")
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+    .then(() => console.log("connected to DB"))
+    .catch((err) => console.log(err));
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+
+app.listen(8080, () => {
+    console.log("app is listning on port 8080");
+});
