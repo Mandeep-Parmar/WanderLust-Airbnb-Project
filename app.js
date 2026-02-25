edb43f98b9bd12ed79fb15e95fb9c7eae87e7727
Mandeep-Parmar <mandeeppar07@gmail.com>
2026-02-25 22:25:21 +0530

    Add express-session

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
+const session = require("express-session");
 
 const listings = require("./routes/listing.js");
 const reviews = require("./routes/review.js");
@@ -17,6 +18,19 @@ app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "public")));
+
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly : true
+    }
+};
+
+app.use(session(sessionOptions));
 
 const MONGO_URL = process.env.MONGO_URL;
 
