fc8b201f2b48ee8669a75d57b167e0448fbae2ef
Mandeep-Parmar <mandeeppar07@gmail.com>
2026-02-28 14:27:16 +0530

    Add user SignUp and Login routes and connect login route

--- app.js
+++ app.js
@@ -11,8 +11,9 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 require("dotenv").config();
 
@@ -66,22 +67,23 @@ app.listen(8080, () => {
     console.log("app is listning on port 8080");
 });
 
-app.get("/demouser", async (req, res) => {
-    let fakeUser = new User ({
-        email: "student@gmail.com",
-        username: "delta-student"
-    });
+// app.get("/demouser", async (req, res) => {
+//     let fakeUser = new User ({
+//         email: "student@gmail.com",
+//         username: "delta-student"
+//     });
 
-    const registeredUser = await User.register(fakeUser, "helloworld");
-    res.send(registeredUser);
-});
+//     const registeredUser = await User.register(fakeUser, "helloworld");
+//     res.send(registeredUser);
+// });
 
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
 });
 
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
 
 // 404 handler
 app.use((req, res, next) => {
