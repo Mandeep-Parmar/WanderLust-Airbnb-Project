aaa5a88586f8dca4860a5fb554d44bdc8b2c800a
Mandeep-Parmar <mandeeppar07@gmail.com>
2026-02-28 14:42:21 +0530

    Add logout route

--- routes/user.js
+++ routes/user.js
@@ -35,4 +35,14 @@ router.post("/login", passport.authenticate("local", {
     }
 );
 
+router.get("/logout", (req, res, next) => {
+    req.logOut((err) => {
+        if(err) {
+            return next(err);
+        }
+        req.flash("success", "You are logged out!");
+        res.redirect("/listings");
+    });
+});
+
 module.exports = router;
