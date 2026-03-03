6072dae0c6e08fd871de449c87455a2a0fff1e12
Mandeep-Parmar <mandeeppar07@gmail.com>
2026-03-03 19:00:25 +0530

    Delete Cloudinary image when listing is removed

--- controllers/listings.js
+++ controllers/listings.js
@@ -40,7 +40,7 @@ module.exports.createListing = async (req, res) => {
   const newListing = new Listing(req.body.listing);
   newListing.owner = req.user._id;
   newListing.image = { url, filename };
-  
+
   await newListing.save();
   req.flash("success", "New Listing Created!");
   res.redirect("/listings");
@@ -60,7 +60,7 @@ module.exports.updateListing = async (req, res) => {
   let { id } = req.params;
   let listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
 
-  if(req.file){
+  if (req.file) {
     // Delete old image from Cloudinary
     await cloudinary.uploader.destroy(listing.image.filename);
 
@@ -69,14 +69,24 @@ module.exports.updateListing = async (req, res) => {
     listing.image = { url, filename };
     await listing.save();
   }
-  
+
   req.flash("success", "Listing Updated!");
   res.redirect(`/listings/${id}`);
 };
 
 module.exports.destroyListing = async (req, res) => {
   let { id } = req.params;
+
+  const listing = await Listing.findById(id);
+
+  // Delete image from Cloudinary
+  if (listing.image && listing.image.filename) {
+    await cloudinary.uploader.destroy(listing.image.filename);
+  }
+
+  // Delete listing from DB
   await Listing.findByIdAndDelete(id);
+
   req.flash("success", "Listing Deleted!");
   res.redirect("/listings");
 };
