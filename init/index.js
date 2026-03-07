b7588c34f1f2cbecf745184a57a20e423d57843b
Mandeep-Parmar <mandeeppar07@gmail.com>
2026-03-07 13:59:59 +0530

    Add geometry for all listings

--- init/index.js
+++ init/index.js
@@ -1,8 +1,9 @@
 require("dotenv").config({ path: "../.env" });
 
 const mongoose = require("mongoose");
-const initData = require("./data.js")
-const Listing = require("../models/listing.js")
+const {data : initData} = require("./data.js");
+const Listing = require("../models/listing.js");
+const geocoder = require("../utils/geocoder"); //node-geocoder file
 
 const MONGO_URL = process.env.MONGO_URL;
 
@@ -16,8 +17,31 @@ async function main() {
 
 const initDB = async () => {
     await Listing.deleteMany({});
-    initData.data = initData.data.map((obj) => ({...obj, owner: "69a3bb98b7b984d430534a4e"}));
-    await Listing.insertMany(initData.data);
+
+    for (let listing of initData) {
+
+        const geoData = await geocoder.geocode(
+        `${listing.location}, ${listing.country}`
+        );
+
+        // check if geocoder returns data
+        if (geoData.length > 0) {
+            listing.geometry = {
+                type: "Point",
+                coordinates: [
+                geoData[0].longitude,
+                geoData[0].latitude
+                ]
+            };
+        }
+
+        listing.owner = new mongoose.Types.ObjectId("69a3bb98b7b984d430534a4e");
+
+        // add delay (VERY IMPORTANT)
+        await new Promise(resolve => setTimeout(resolve, 1000));
+    }
+
+    await Listing.insertMany(initData);
     console.log("Data was initialized");
 }
 
